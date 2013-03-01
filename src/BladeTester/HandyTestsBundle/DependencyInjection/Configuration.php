188d0bc463125f8a9b4c18ffc932745f5ee5d1cc
Carles Climent <carlescliment@hotmail.com>
2013-03-01 13:05:44 +0100

    Created Configuration class

---
+++ src/BladeTester/HandyTestsBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BladeTester\HandyTestsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('handy_tests.factory_girl');
+        return $treeBuilder;
+    }
+
+}
